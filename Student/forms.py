62f0e3fe573db205b0e26b966ef05ca37ea22097
Karishma <kshinde.sawant@gmail.com>
2020-12-29 16:56:54 +0530

    Student login form Added

---
+++ Student/forms.py
@@ -0,0 +1,63 @@
+from django import forms 
+from django.forms import Form
+from Student.models import Courses
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class AddStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+
+    #For Displaying Courses
+    try:
+        courses = Courses.objects.all()
+        course_list = []
+        for course in courses:
+            single_course = (course.id, course.course_name)
+            course_list.append(single_course)
+    except:
+        course_list = []
+  
+    gender_list = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+    
+    course_id = forms.ChoiceField(label="Course", choices=course_list, widget=forms.Select(attrs={"class":"form-control"}))
+    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
+
+
+
+class EditStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+
+    #For Displaying Courses
+    try:
+        courses = Courses.objects.all()
+        course_list = []
+        for course in courses:
+            single_course = (course.id, course.course_name)
+            course_list.append(single_course)
+    except:
+        course_list = []
+
+    
+    gender_list = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+    
+    course_id = forms.ChoiceField(label="Course", choices=course_list, widget=forms.Select(attrs={"class":"form-control"}))
+    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
+   
