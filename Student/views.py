a45f9095cdd70ec3e73475544c9f2cba484147f0
Karishma <kshinde.sawant@gmail.com>
2020-12-29 17:00:35 +0530

    Admin Update Profil view Createde

--- Student/views.py
+++ Student/views.py
@@ -1,3 +1,157 @@
-from django.shortcuts import render
 
-# Create your views here.
+# from channels.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from Student.models import CustomUser,Courses,Students,Admin
+from Student.emailbackend import EmailBackEnd
+
+
+def loginPage(request):
+    return render(request, 'login.html')
+
+
+def Login(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            user_type = user.user_type
+            #return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
+            if user_type == '1':
+                return redirect('admin_home')
+                
+            elif user_type == '2':
+                # return HttpResponse("Student Login")
+                return redirect('student_profile')
+            else:
+                messages.error(request, "Invalid Login!")
+                return redirect('login')
+        else:
+            messages.error(request, "Invalid Login Credentials!")
+            #return HttpResponseRedirect("/")
+            return redirect('login')
+
+
+def get_user_details(request):
+    if request.user != None:
+        return HttpResponse("User: "+request.user.email+" User Type: "+request.user.user_type)
+    else:
+        return HttpResponse("Please Login First")
+
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+
+def student_profile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    student = Students.objects.get(admin=user)
+
+    context={
+        "user": user,
+        "student": student
+    }
+    return render(request, 'student_template/student_profile.html', context)
+
+
+def student_profile_update(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('student_profile')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+            if password != None and password != "":
+                customuser.set_password(password)
+            customuser.save()
+
+            student = Students.objects.get(admin=customuser.id)
+            student.address = address
+            student.save()
+            
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('student_profile')
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return redirect('student_profile')
+        
+        
+def admin_home(request):
+    all_student_count = Students.objects.all().count()
+    course_count = Courses.objects.all().count()
+
+    # Total Subjects and students in Each Course
+    course_all = Courses.objects.all()
+    course_name_list = []
+    subject_count_list = []
+    student_count_list_in_course = []
+
+    for course in course_all:
+        students = Students.objects.filter(course_id=course.id).count()
+        course_name_list.append(course.course_name)
+        student_count_list_in_course.append(students)
+    
+
+
+    # For Students
+    student_name_list=[]
+
+    students = Students.objects.all()
+    for student in students:
+        student_name_list.append(student.admin.first_name)
+
+
+    context={
+        "all_student_count": all_student_count,
+        "course_count": course_count,
+        "course_name_list": course_name_list,
+        "subject_count_list": subject_count_list,
+        "student_count_list_in_course": student_count_list_in_course,
+        "student_name_list": student_name_list,
+    }
+    return render(request, "admin_template/home.html", context)
+
+def admin_profile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+
+    context={
+        "user": user
+    }
+    return render(request, 'admin_template/admin_profile.html', context)
+
+
+def admin_profile_update(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('admin_profile')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+            if password != None and password != "":
+                customuser.set_password(password)
+            customuser.save()
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('admin_profile')
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return redirect('admin_profile')
