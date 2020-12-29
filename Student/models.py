63b0846734456d6d9104f70b4e16cf9fa4c03dae
Karishma <kshinde.sawant@gmail.com>
2020-12-29 16:56:04 +0530

    Admin model Created

--- Student/models.py
+++ Student/models.py
@@ -1,34 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@KARISHMASHINDE 
-vijaythapa333
-/
-django-student-management-system
-2
-3433
-Code
-Issues
-Pull requests
-Actions
-Projects
-1
-Wiki
-Security
-Insights
-django-student-management-system/student_management_app/models.py /
-@vijaythapa333
-vijaythapa333 Add Result on Staff Panel Complete
-Latest commit 15058f2 on Jul 7
- History
- 1 contributor
-199 lines (150 sloc)  7.19 KB
-  
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
