524dbcbe4d27d6844ee6c05adee5159fbb99f53f
Karishma <kshinde.sawant@gmail.com>
2020-12-30 12:13:12 +0530

    Crispy forms added in Setting.py

--- StudentManagement/settings.py
+++ StudentManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Student'
+    'Student',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +83,8 @@ DATABASES = {
     }
 }
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL ='student_list"'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
